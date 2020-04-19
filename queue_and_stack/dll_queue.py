d6a15440ea1ab97291bacf9ad866c0ed9a8c5849
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-04-18 20:39:10 -0700

    queues and stacks almost done, getting one error for both

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,6 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
@@ -8,12 +8,22 @@ class Queue:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.queue = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # increment the size of the queue
+
+        self.queue.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.queue.remove_from_head()
+            self.size -= 1
+
+        else:
+            return None
 
     def len(self):
-        pass
+        self.size = len(self.queue)
+        return self.size
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,29 @@
+from doubly_linked_list import DoublyLinkedList
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+
+# LAST IN, FIRST OUT
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.stack = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.stack.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.stack.remove_from_tail()
+            self.size -= 1
+
+        else:
+            return None
 
     def len(self):
-        pass
+        self.size = len(self.stack)
+        return self.size
